fdccb46d2f3c1006a78741327ff88af2d20a684b
Matt Tucker <matt.tucker@colorado.edu>
2012-02-07 15:15:49 -0700

    Additional settings for min width, margin and adjustment.

--- bs.js
+++ bs.js
@@ -1,3 +1,12 @@
 (function ($) {
-  $.backstretch(Drupal.settings.backstretchURL);
+  if ($(window).width() > Drupal.settings.backstretchMinWidth) {
+    $.backstretch(Drupal.settings.backstretchURL);
+    
+    $(document).ready(function () { 
+      //var height = $(window).height() - 153;
+      var height = $(window).height() + parseInt(Drupal.settings.backstretchMarginAdj);
+      if($('#toolbar').length > 0) { var height = height - $('#toolbar').height(); }
+      $(Drupal.settings.backstretchSelector).css('margin-bottom', height);
+    });
+  }
 })(jQuery);
