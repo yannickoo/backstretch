3226bb1783ad3d6f7f23b004479b494045d0143c
Matt Tucker <matt.tucker@colorado.edu>
2012-03-04 18:09:26 -0700

    Cleanup admin form a tad with fieldsets.

--- bs.js
+++ bs.js
@@ -3,7 +3,6 @@
     $.backstretch(Drupal.settings.backstretchURL);
     
     $(document).ready(function () { 
-      //var height = $(window).height() - 153;
       var height = $(window).height() + parseInt(Drupal.settings.backstretchMarginAdj);
       if($('#toolbar').length > 0) { var height = height - $('#toolbar').height(); }
       $(Drupal.settings.backstretchSelector).css('margin-bottom', height);
