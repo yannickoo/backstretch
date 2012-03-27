40b383a18ba26bc8b38dc1da2a34d01eba7af94d
Matt Tucker <matt.tucker@colorado.edu>
2012-03-26 19:16:29 -0600

    Changing name of waypoints to 'scroller' and making it much more automatic - no more need to alter theme. Cleaned up a few docs along the way

--- bs.js
+++ bs.js
@@ -3,9 +3,15 @@
     $.backstretch(Drupal.settings.backstretchURL);
     
     $(document).ready(function () { 
-      var height = $(window).height() + parseInt(Drupal.settings.backstretchMarginAdj);
-      if($('#toolbar').length > 0) { var height = height - $('#toolbar').height(); }
-      $(Drupal.settings.backstretchSelector).css('margin-bottom', height);
+      if (Drupal.settings.backstretchScroller) {
+        var height = $(window).height() + parseInt(Drupal.settings.backstretchScrollerAdj);
+        if($('#toolbar').length > 0) { var height = height - $('#toolbar').height(); }
+        
+        if (Drupal.settings.backstretchScrollerAdj != 0) {
+          $('body').append('<div id="backstretchmargin"></div>');
+          $('#backstretchmargin').css('margin-bottom', height);
+        }
+      }
     });
   }
 })(jQuery);
