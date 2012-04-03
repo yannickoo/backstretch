318c2107b49530d4f8d17402fe55ffb05132def9
ultimateboy <ultimateboy@gmail.com>
2012-04-03 11:41:49 -0600

    Adding scrollto and waypoints functionality.

--- bs.js
+++ bs.js
@@ -11,6 +11,30 @@
         }
         $('#backstretchmargin').css('margin-bottom', height);
       }
+
+      if (Drupal.settings.backstretchScrollTo) {
+
+        $("#footer").waypoint(function(){
+          $("#backstretch-scrollto").text("Back to Top").attr("href", "#page");
+        });
+
+        $("#main-menu").waypoint(function(){
+          $("#backstretch-scrollto").text("View Photo").attr("href", "#backstretchmargin");
+        });
+        
+        // Scroll to right place on click.
+        $("#backstretch-scrollto").click(function(e){
+          var s = $(this).attr("href");
+          console.log(s);
+          if (s == '#backstretchmargin') {
+            var s = 'max';
+            console.log('hi');
+          }
+          $.scrollTo(s, 1000);
+          e.preventDefault();
+          return false;
+        });
+      }
     });
   }
 })(jQuery);
